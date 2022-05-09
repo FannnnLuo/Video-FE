27e78752a31e0ae378ab0f917b7a822d50500fc8
FannnWho <fluo@beaconfireinc.com>
2022-05-08 21:57:12 -0400

    access denied

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import VideoCall from './componenet/VideoCall';
 function App() {
   return (
     <div className="App">
+      <h1>HI</h1>
       <Routes>
         <Route path='/login' element={ <Login/>} />
         <Route path='/video-call' element={<VideoCall/> } />
