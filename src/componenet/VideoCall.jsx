19ccc0ac84986b335a9042e28df44207ff11a1b6
FannnWho <fluo@beaconfireinc.com>
2022-05-08 21:47:03 -0400

    change be to heroku

--- src/componenet/VideoCall.jsx
+++ src/componenet/VideoCall.jsx
@@ -15,7 +15,7 @@ import Peer from "simple-peer"
 // });
 //
 
-const manager = new Manager('https://192.168.84.25:8000', {rejectUnauthorized: false, transports: ["websocket"]})
+const manager = new Manager('https://video-app-react-be.herokuapp.com/', {rejectUnauthorized: false, transports: ["websocket"]})
 const socket = manager.socket('/')
 
 manager.on('error', (error) => {
